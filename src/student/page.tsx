d80b356aab8f43bc5a3cc632a39d81d63171e976
zainal-Anhsin <133510409+pongnal@users.noreply.github.com>
2025-06-24 15:53:13 +0800

    upload student page
    all complete

--- src/student/page.tsx
+++ src/student/page.tsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React from "react";
+import { Space, Breadcrumb } from "antd";
+import {
+  EditOutlined,
+  SearchOutlined,
+  UploadOutlined,
+  FileAddOutlined,
+} from "@ant-design/icons";
+import { SectionTitle } from "../components/common/Text/Text";
+import {
+  ButtonMediumWhitePurple,
+} from "../components/common/Button/Button";
 
-const page = () => {
+const Student = () => {
   return (
     <div>
-        <h1 style={{color: 'black'}}>Student Page</h1>
+      {/* Breadcrumb section */}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: 12,
+          marginBottom: 24,
+        }}
+      >
+        <SectionTitle style={{ marginRight: 8 }}>Student</SectionTitle>
+        <span style={{ fontWeight: 400, color: "black", fontSize: 14 }}>|</span>
+        <Breadcrumb separator=">">
+          <Breadcrumb.Item>
+            <a href="/dashboard" style={{ color: "#8c98a4" }}>
+              Home
+            </a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="/student" style={{ color: "#222", fontWeight: 600 }}>
+              Student
+            </a>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      {/* Buttons Section */}
+      <div style={{ background: "#f2f6fe", minHeight: "2vh", padding: 32 }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          {/* Left side: Remove, Profile, Switch */}
+          <Space>
+            <ButtonMediumWhitePurple
+              style={{ background: "#7c3aed", color: "white" }}
+            >
+              <EditOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Bulk Edit
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple>
+              <EditOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Custom Group
+            </ButtonMediumWhitePurple>
+          </Space>
+          {/* Right side: Cancel, Reset, Save */}
+          <Space>
+            <ButtonMediumWhitePurple>
+              <SearchOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Search Student
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple>
+              <UploadOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Upload Student
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple>
+              <FileAddOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Add Student
+            </ButtonMediumWhitePurple>
+          </Space>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Student;
--- src/student/uploadStudent/page.tsx
+++ src/student/uploadStudent/page.tsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { Row, Col, Space, Breadcrumb } from "antd";
-import { SaveOutlined, UndoOutlined, CloseOutlined } from "@ant-design/icons";
-import { Form, FormItem, FormInput } from "../../components/common/Form/Form";
+import { Space, Breadcrumb, message, Upload } from "antd";
+import {
+  SaveOutlined,
+  UndoOutlined,
+  CloseOutlined,
+  InboxOutlined,
+} from "@ant-design/icons";
 import { HeadingLg, SectionTitle } from "../../components/common/Text/Text";
 import Divider from "../../components/common/Divider/Divider";
-import {
-  DropdownGrey,
-  DropdownOption,
-} from "../../components/common/Dropdown/Dropdown";
 import {
   ButtonMediumWhitePurple,
   ButtonMediumGrey,
 } from "../../components/common/Button/Button";
+const { Dragger } = Upload;
 
 const uploadStudent = () => {
+  const uploadProps = {
+    name: "file",
+    multiple: true,
+    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
+    onChange(info: any) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e: React.DragEvent<HTMLDivElement>) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+
   return (
     <div>
       {/* Breadcrumb section */}
@@ -26,9 +47,7 @@ const uploadStudent = () => {
         }}
       >
         <SectionTitle style={{ marginRight: 8 }}>Student</SectionTitle>
-        <span style={{ fontWeight: 400, color: "black", fontSize: 14 }}>
-          |
-        </span>
+        <span style={{ fontWeight: 400, color: "black", fontSize: 14 }}>|</span>
         <Breadcrumb separator=">">
           <Breadcrumb.Item>
             <a href="/dashboard" style={{ color: "#8c98a4" }}>
@@ -41,7 +60,9 @@ const uploadStudent = () => {
             </a>
           </Breadcrumb.Item>
           <Breadcrumb.Item>
-            <span style={{ color: "#222", fontWeight: 600 }}>Upload Student</span>
+            <span style={{ color: "#222", fontWeight: 600 }}>
+              Upload Student Profile
+            </span>
           </Breadcrumb.Item>
         </Breadcrumb>
       </div>
@@ -82,50 +103,98 @@ const uploadStudent = () => {
           </Space>
         </div>
       </div>
-      {/* Add student form section*/}
       <div style={{ display: "grid", placeItems: "center", marginTop: 24 }}>
-        <HeadingLg
+        <div
           style={{
-            marginBottom: 22,
             justifyContent: "flex-start",
             width: "100%",
             maxWidth: "800px",
           }}
         >
-          Add New Student
-        </HeadingLg>
-
-        <Form style={{ maxWidth: "800px" }}>
-          <Row gutter={32}>
-            <Col span={12}>
-              <FormItem label="Student Name as per IC" name="name">
-                <FormInput placeholder="Name" />
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Email" name="email">
-                <FormInput placeholder="Email Address" />
-              </FormItem>
-            </Col>
-          </Row>
-          <Row gutter={32}>
-            <Col span={12}>
-              <FormItem label="Group" name="group">
-                <DropdownGrey placeholder="Select">
-                  <DropdownOption value="A">Group A</DropdownOption>
-                  <DropdownOption value="B">Group B</DropdownOption>
-                  <DropdownOption value="C">Group C</DropdownOption>
-                </DropdownGrey>
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Classroom" name="classroom">
-                <FormInput placeholder="Classroom Name" />
-              </FormItem>
-            </Col>
-          </Row>
-        </Form>
-
+          <HeadingLg style={{ marginBottom: 22, marginTop: 22 }}>
+            Upload Student Profile
+          </HeadingLg>
+          <SectionTitle>
+            <span style={{ color: "#7c3aed", cursor: "pointer" }}>
+              Download & use the provided spreadsheet template file
+            </span>{" "}
+            for upload purpose.
+            <br />
+            <span>
+              Using other files with different column name or format will cause
+              errors during data validation.
+            </span>
+          </SectionTitle>
+        </div>
+        {/* Dragger upload section */}
+        <div
+          style={{ width: "100%", maxWidth: 800, margin: "32px auto 0 auto" }}
+        >
+          <Dragger {...uploadProps} style={{ padding: 32 }}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined
+                style={{ color: "#7c3aed" }}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+            </p>
+            <p
+              className="ant-upload-text"
+              style={{ color: "#7c3aed", fontWeight: 500, fontSize: 18 }}
+            >
+              Drop files to upload or
+            </p>
+            <p>
+              <button
+                type="button"
+                style={{
+                  background: "#7c3aed",
+                  color: "#fff",
+                  border: "none",
+                  borderRadius: 6,
+                  padding: "8px 32px",
+                  fontWeight: 500,
+                  fontSize: 18,
+                  cursor: "pointer",
+                }}
+              >
+                Browse
+              </button>
+            </p>
+          </Dragger>
+        </div>
+        {/* Checkbox section */}
+        <div
+          style={{
+            width: "100%",
+            maxWidth: 800,
+            margin: "32px auto 0 auto",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: 16,
+          }}
+        >
+          <label style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <input
+              type="checkbox"
+              style={{ width: 20, height: 20, accentColor: "#7c3aed" }}
+            />
+            <SectionTitle style={{ fontWeight: 400 }}>
+              Send email after upload
+            </SectionTitle>
+          </label>
+          <label style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <input
+              type="checkbox"
+              defaultChecked
+              style={{ width: 20, height: 20, accentColor: "#7c3aed" }}
+            />
+            <SectionTitle style={{ fontWeight: 400 }}>
+              Reset Password after upload
+            </SectionTitle>
+          </label>
+        </div>
         {/* Divider section */}
         <Divider />
       </div>
