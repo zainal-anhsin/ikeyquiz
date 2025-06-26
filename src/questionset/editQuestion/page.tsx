fb7f1aba81ed01dd8a5cc4e6df6766e69524d309
zainal <133510409+pongnal@users.noreply.github.com>
2025-06-26 19:03:16 +0800

    update page edit question

--- src/questionset/editQuestion/page.tsx
+++ src/questionset/editQuestion/page.tsx
@@ -1,11 +1,208 @@
-import React from 'react'
+import React from "react";
+import { Space, Breadcrumb, message, Upload, Row, Col } from "antd";
+import {
+  SaveOutlined,
+  UndoOutlined,
+  CloseOutlined,
+  InboxOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import {
+  HeadingLg,
+  MetaText,
+  SectionTitle,
+} from "../../components/common/Text/Text";
+import {
+  ButtonMediumWhitePurple,
+  ButtonMediumGrey,
+} from "../../components/common/Button/Button";
+import { Form, FormItem, FormInput } from "../../components/common/Form/Form";
+import {
+  DropdownGrey,
+  DropdownOption,
+} from "../../components/common/Dropdown/Dropdown";
+const { Dragger } = Upload;
 
-const page = () => {
+const editQuestion = () => {
+  const uploadProps = {
+    name: "file",
+    multiple: true,
+    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
+    onChange(info: any) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e: React.DragEvent<HTMLDivElement>) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+
   return (
     <div>
-        <h1 style={{color: 'black'}}>Edit Page</h1>
+      {/* Breadcrumb section */}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: 12,
+          marginBottom: 24,
+        }}
+      >
+        <SectionTitle style={{ marginRight: 8 }}>Question Set</SectionTitle>
+        <span style={{ fontWeight: 400, color: "black", fontSize: 14 }}>|</span>
+        <Breadcrumb separator=">">
+          <Breadcrumb.Item>
+            <a href="/dashboard" style={{ color: "#8c98a4" }}>
+              Home
+            </a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="/questionset" style={{ color: "#8c98a4" }}>
+              Question Set
+            </a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <span style={{ color: "#222", fontWeight: 600 }}>
+              Edit Question Set
+            </span>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      {/* Buttons Section */}
+      <div style={{ background: "#f2f6fe", minHeight: "2vh", padding: 32 }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          {/* Left side: Remove, Profile, Switch */}
+          <Space>
+            <ButtonMediumWhitePurple>
+              <DeleteOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Delete
+            </ButtonMediumWhitePurple>
+          </Space>
+          {/* Right side: Cancel, Reset, Save */}
+          <Space>
+            <ButtonMediumGrey>
+              <CloseOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Cancel
+            </ButtonMediumGrey>
+            <ButtonMediumWhitePurple>
+              <UndoOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Reset
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple
+              style={{ background: "#7c3aed", color: "white" }}
+            >
+              <SaveOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Save
+            </ButtonMediumWhitePurple>
+          </Space>
+        </div>
+      </div>
+      <div style={{ display: "grid", placeItems: "center", marginTop: 24 }}>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            width: "100%",
+            maxWidth: "800px",
+          }}
+        >
+          <HeadingLg style={{ marginBottom: 22, marginTop: 22 }}>
+            Edit Question Set
+          </HeadingLg>
+          <Form style={{ maxWidth: "800px" }}>
+            <Row gutter={32}>
+              <Col span={12}>
+                <FormItem label="Name" name="name">
+                  <FormInput placeholder="Enter Name" />
+                </FormItem>
+              </Col>
+              <Col span={12}>
+                <FormItem label="Display Order" name="displayOrder">
+                  <FormInput placeholder="0" />
+                </FormItem>
+              </Col>
+            </Row>
+            <Row gutter={32}>
+              <Col span={12}>
+                <FormItem label="Remarks (Public)" name="remarks">
+                  <FormInput.TextArea
+                    placeholder=""
+                    style={{ height: "100px", resize: "vertical" }}
+                  />
+                </FormItem>
+              </Col>
+              <Col span={12}>
+                <FormItem label="Total Question" name="totalQuestion">
+                  <FormInput
+                    type="number"
+                    defaultValue={30}
+                    placeholder="Enter number"
+                  />
+                </FormItem>
+              </Col>
+            </Row>
+            <Row gutter={32}>
+              <Col span={12}>
+              <FormItem label="Group" name="group">
+                <DropdownGrey placeholder="Select">
+                  <DropdownOption value="A">Group A</DropdownOption>
+                  <DropdownOption value="B">Group B</DropdownOption>
+                  <DropdownOption value="C">Group C</DropdownOption>
+                </DropdownGrey>
+              </FormItem>
+              </Col>
+            </Row>
+            <Row gutter={32}>
+              <Col span={12}>
+              <FormItem label="Subject" name="subject">
+                <DropdownGrey placeholder="Select">
+                  <DropdownOption value="A">Subject A</DropdownOption>
+                  <DropdownOption value="B">Subject B</DropdownOption>
+                  <DropdownOption value="C">Subject C</DropdownOption>
+                </DropdownGrey>
+              </FormItem>
+              </Col>
+            </Row>
+            <Row gutter={32}>
+              <Col span={12}>
+                <FormItem label="Remarks (Hidden)" name="remarksHidden">
+                  <FormInput.TextArea
+                    placeholder=""
+                    style={{ height: "100px", resize: "vertical" }}
+                  />
+                </FormItem>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default editQuestion;
--- src/questionset/page.tsx
+++ src/questionset/page.tsx
@@ -1,11 +1,271 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Space, Breadcrumb, Select } from "antd";
+import type { TableColumnsType } from "antd";
+import {
+  EditOutlined,
+  SearchOutlined,
+  UploadOutlined,
+  FileAddOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import { SectionTitle } from "../components/common/Text/Text";
+import { ButtonMediumWhitePurple } from "../components/common/Button/Button";
+import Checkbox from "../components/common/Checkbox/Checkbox";
+import { useNavigate } from "react-router-dom";
+import Table from "../components/common/Table/Table";
+import { tableData, DataType } from "../student/tableData";
+import { DropdownGrey, DropdownOption } from "../components/common/Dropdown/Dropdown";
 
-const page = () => {
+const QuestionSet = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const navigate = useNavigate();
+
+  const onSelectChange = (
+    newSelectedRowKeys: React.Key[],
+    selectedRows: DataType[]
+  ) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    console.log("selectedRows: ", selectedRows);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "#",
+      dataIndex: "index",
+      render: (_: any, __: DataType, index: number) => index + 1,
+      width: 60,
+    },
+    { title: "Name", dataIndex: "name" },
+    {
+      title: "Custom Group",
+      dataIndex: "customGroup",
+      render: (_: any, record: DataType) => (
+        <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
+          <label style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <Checkbox checked={record.customGroup.cg1} />
+            <span
+              style={{
+                fontFamily: "'Inter', 'Roboto', 'Arial', sans-serif",
+                fontSize: 16,
+                fontWeight: 400,
+                color: "#222",
+              }}
+            >
+              CG 1
+            </span>
+          </label>
+          <label style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <Checkbox checked={record.customGroup.cg2} />
+            <span
+              style={{
+                fontFamily: "'Inter', 'Roboto', 'Arial', sans-serif",
+                fontSize: 16,
+                fontWeight: 400,
+                color: "#222",
+              }}
+            >
+              CG 2
+            </span>
+          </label>
+        </div>
+      ),
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      render: (_: any, record: DataType) => (
+        <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
+          <label style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <Checkbox checked={record.category.liveQuiz} />
+            <span
+              style={{
+                fontFamily: "'Inter', 'Roboto', 'Arial', sans-serif",
+                fontSize: 16,
+                fontWeight: 400,
+                color: "#222",
+              }}
+            >
+              Live Quiz
+            </span>
+          </label>
+          <label style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <Checkbox checked={record.category.kosaKata} />
+            <span
+              style={{
+                fontFamily: "'Inter', 'Roboto', 'Arial', sans-serif",
+                fontSize: 16,
+                fontWeight: 400,
+                color: "#222",
+              }}
+            >
+              Kosa Kata Spot+ (full)
+            </span>
+          </label>
+        </div>
+      ),
+    },
+    { title: "Classroom", dataIndex: "classroom" },
+    { title: "Group", dataIndex: "group" },
+    { title: "Email", dataIndex: "email" },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: () => (
+        <ButtonMediumWhitePurple
+          onClick={() => navigate("/questionset/edit-question")}
+        >
+          Edit
+        </ButtonMediumWhitePurple>
+      ),
+    },
+  ];
+
   return (
     <div>
-        <h1 style={{color: 'black'}}>Question Set Page</h1>
-    </div>
-  )
-}
+      {/* Breadcrumb section */}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: 12,
+          marginBottom: 24,
+        }}
+      >
+        <SectionTitle style={{ marginRight: 8 }}>Question Set</SectionTitle>
+        <span style={{ fontWeight: 400, color: "black", fontSize: 14 }}>|</span>
+        <Breadcrumb separator=">">
+          <Breadcrumb.Item>
+            <a href="/dashboard" style={{ color: "#8c98a4" }}>
+              Home
+            </a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="/questionset" style={{ color: "#222", fontWeight: 600 }}>
+              Question Set
+            </a>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      {/* Buttons Section */}
+      <div style={{ background: "#f2f6fe", minHeight: "2vh", padding: 32 }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          {/* Left side: Remove, Profile, Switch */}
+          <Space>
+            <ButtonMediumWhitePurple
+              style={{ background: "#7c3aed", color: "white", width: 120 }}
+            >
+              <EditOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Bulk Edit
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple style={{ width: 170 }}>
+              <DeleteOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Bulk Remove
+            </ButtonMediumWhitePurple>
+          </Space>
+          {/* Right side: Cancel, Reset, Save */}
+          <Space>
+            <ButtonMediumWhitePurple style={{ width: 220 }}>
+              <SearchOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Search Question Set
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple style={{ width: 220 }}
+              onClick={() => navigate("/questionset/upload-question")}
+            >
+              <UploadOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Upload Question Set
+            </ButtonMediumWhitePurple>
+            <ButtonMediumWhitePurple style={{ width: 120 }}
+              onClick={() => navigate("")}
+            >
+              <FileAddOutlined
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              />
+              Create
+            </ButtonMediumWhitePurple>
+          </Space>
+        </div>
+      </div>
 
-export default page
+      {/* Table Section */}
+      <div style={{ marginTop: 34 }}>
+        <div style={{ marginBottom: 18, display: 'flex', alignItems: 'center', gap: 16, justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
+            <SectionTitle style={{ marginRight: 8 }}>Filter</SectionTitle>
+            <Select
+              mode="tags"
+              size="large"
+              style={{ width: 160 }}
+              placeholder="Custom Group"
+              options={[
+                { value: 'CG 1', label: 'CG 1' },
+                { value: 'CG 2', label: 'CG 2' },
+              ]}
+              onChange={(value) => console.log('selected', value)}
+            />
+            <Select
+              mode="tags"
+              size="large"
+              style={{ width: 220 }}
+              placeholder="Category"
+              options={[
+                { value: 'Live Quiz', label: 'Live Quiz' },
+                { value: 'Kosa Kata Spot+ (full)', label: 'Kosa Kata'},
+              ]}
+              onChange={(value) => console.log('selected', value)}
+            />
+            <DropdownGrey placeholder="Classroom" size="large" style={{ width: 140 }} onChange={value => console.log('classroom', value)}>
+              <DropdownOption value="SSR">SSR</DropdownOption>
+              <DropdownOption value="SER">SER</DropdownOption>
+            </DropdownGrey>
+            <DropdownGrey placeholder="Group" size="large" style={{ width: 130 }} onChange={value => console.log('group', value)}>
+              <DropdownOption value="Standard 1">Standard 1</DropdownOption>
+              <DropdownOption value="Standard 2">Standard 2</DropdownOption>
+              <DropdownOption value="Standard 3">Standard 3</DropdownOption>
+            </DropdownGrey>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+            <SectionTitle style={{ marginRight: 8 }}>Sort By:</SectionTitle>
+            <DropdownGrey placeholder="Select" size="large" style={{ width: 115 }} onChange={value => console.log('sort', value)}>
+              <DropdownOption value="name-asc">A - Z</DropdownOption>
+              <DropdownOption value="name-desc">Z - A</DropdownOption>
+            </DropdownGrey>
+          </div>
+        </div>
+        <Table<DataType>
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={tableData}
+          rowKey="key"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuestionSet;
